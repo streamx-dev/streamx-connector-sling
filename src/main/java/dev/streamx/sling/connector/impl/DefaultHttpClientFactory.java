0ccab5c9808e04fe75e520d0379307126d4cadf5
Herman Ciechanowiec <herman.ciechanowiec@ds.pl>
2025-02-10 12:45:52 +0100

    DXP-1626: Support TLS disablement (#27)
    * DXP-1626: Support TLS disablement
    
    * DXP-1626: Refine the code according to the review
    
    * DXP-1626: Refine the code according to the review

--- src/main/java/dev/streamx/sling/connector/impl/DefaultHttpClientFactory.java
+++ src/main/java/dev/streamx/sling/connector/impl/DefaultHttpClientFactory.java
@@ -1,46 +1,117 @@
 package dev.streamx.sling.connector.impl;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.net.ssl.SSLContext;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.osgi.services.HttpClientBuilderFactory;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component(service = DefaultHttpClientFactory.class)
 @Designate(ocd = HttpClientProviderConfig.class)
 public class DefaultHttpClientFactory {
 
-  @Reference
-  private HttpClientBuilderFactory httpClientBuilderFactory;
+  private static final Logger LOG = LoggerFactory.getLogger(DefaultHttpClientFactory.class);
 
-  private HttpClientProviderConfig config;
+  private final HttpClientBuilderFactory httpClientBuilderFactory;
+  private final AtomicReference<HttpClientProviderConfig> config;
 
   @Activate
-  @Modified
-  private void activate(HttpClientProviderConfig config) {
-    this.config = config;
+  public DefaultHttpClientFactory(
+      @Reference(cardinality = ReferenceCardinality.MANDATORY)
+      HttpClientBuilderFactory httpClientBuilderFactory,
+      HttpClientProviderConfig config
+  ) {
+    this.httpClientBuilderFactory = httpClientBuilderFactory;
+    this.config = new AtomicReference<>(config);
   }
 
-  public CloseableHttpClient createNewClient() {
-    PoolingHttpClientConnectionManager connMgr = new PoolingHttpClientConnectionManager();
-    connMgr.setMaxTotal(config.max_total());
-    connMgr.setDefaultMaxPerRoute(config.max_per_route());
+  @Modified
+  private void configure(HttpClientProviderConfig config) {
+    this.config.set(config);
+  }
+
+  private Optional<LayeredConnectionSocketFactory> withTrustAllTlsCertificates() {
+    try {
+      LOG.trace("Building {}", LayeredConnectionSocketFactory.class.getName());
+      TrustStrategy trustStrategy = (chain, authType) -> true;
+      SSLContext sslContext = SSLContextBuilder.create()
+          .loadTrustMaterial(null, trustStrategy)
+          .build();
+      return Optional.of(new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE));
+    } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException exception) {
+      String message = String.format(
+          "Could not create %s", LayeredConnectionSocketFactory.class.getName()
+      );
+      LOG.error(message, exception);
+      return Optional.empty();
+    }
+  }
+
+  private PoolingHttpClientConnectionManager connectionManager() {
+    boolean doTrustAllTlsCerts = config.get().insecure();
+    PoolingHttpClientConnectionManager connMgr;
+    if (doTrustAllTlsCerts) {
+      connMgr = withTrustAllTlsCertificates()
+          .map(
+              sslSocketFactory -> {
+                PlainConnectionSocketFactory plainSocketFactory
+                    = PlainConnectionSocketFactory.getSocketFactory();
+                return RegistryBuilder.<ConnectionSocketFactory>create()
+                    .register("http", plainSocketFactory)
+                    .register("https", sslSocketFactory)
+                    .build();
+              }
+          ).map(
+              registry -> {
+                LOG.debug("Using custom connection manager");
+                return new PoolingHttpClientConnectionManager(registry);
+              }
+          ).orElseThrow();
+    } else {
+      connMgr = new PoolingHttpClientConnectionManager();
+    }
+    connMgr.setMaxTotal(config.get().max_total());
+    connMgr.setDefaultMaxPerRoute(config.get().max_per_route());
+    return connMgr;
+  }
+
+  CloseableHttpClient createNewClient() {
+    HttpClientProviderConfig unwrappedConfig = config.get();
     return httpClientBuilderFactory.newBuilder()
-        .setConnectionManager(connMgr)
-        .setKeepAliveStrategy((response, context) -> config.keep_alive_time())
-        .evictIdleConnections(config.idle_connection_keep_alive_time(), TimeUnit.MILLISECONDS)
-        .evictExpiredConnections()
+        .setConnectionManager(connectionManager())
+        .setKeepAliveStrategy(
+            (response, context) -> unwrappedConfig.keep_alive_time()
+        )
+        .evictIdleConnections(
+            unwrappedConfig.idle_connection_keep_alive_time(), TimeUnit.MILLISECONDS
+        ).evictExpiredConnections()
         .setDefaultRequestConfig(RequestConfig.custom()
-            .setConnectTimeout(config.connection_timeout())
-            .setSocketTimeout(config.socket_timeout())
-            .setConnectionRequestTimeout(config.connection_request_timeout())
+            .setConnectTimeout(unwrappedConfig.connection_timeout())
+            .setSocketTimeout(unwrappedConfig.socket_timeout())
+            .setConnectionRequestTimeout(unwrappedConfig.connection_request_timeout())
             .build())
         .build();
   }
-
 }
