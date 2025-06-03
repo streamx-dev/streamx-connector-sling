5d5385f09d2c6456349b089ea28e2c5a8b09089e
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-06-03 12:26:31 +0200

    Remove protected method

--- src/test/java/dev/streamx/sling/connector/impl/StreamxClientFactoryImplTest.java
+++ src/test/java/dev/streamx/sling/connector/impl/StreamxClientFactoryImplTest.java
@@ -1,18 +1,16 @@
 package dev.streamx.sling.connector.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.mock;
 
-import dev.streamx.clients.ingestion.exceptions.StreamxClientException;
 import java.lang.annotation.Annotation;
-import org.apache.http.impl.client.HttpClients;
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.Test;
 
 class StreamxClientFactoryImplTest {
 
   @Test
-  void shouldCreateStreamxClientFactory() throws StreamxClientException {
+  void shouldCreateStreamxClientFactory() throws Exception {
     // given
     StreamxClientConfigOcd config = new StreamxClientConfigOcd() {
 
@@ -45,8 +43,10 @@ class StreamxClientFactoryImplTest {
     };
 
     // when
-    StreamxClientFactoryImpl streamxClientFactory = spy(new StreamxClientFactoryImpl());
-    doReturn(HttpClients.createDefault()).when(streamxClientFactory).createNewClient();
+    StreamxClientFactoryImpl streamxClientFactory = new StreamxClientFactoryImpl();
+    Field defaultHttpClientFactoryField = streamxClientFactory.getClass().getDeclaredField("defaultHttpClientFactory");
+    defaultHttpClientFactoryField.setAccessible(true);
+    defaultHttpClientFactoryField.set(streamxClientFactory, mock(DefaultHttpClientFactory.class));
 
     StreamxInstanceClient streamxClient = streamxClientFactory
         .createStreamxClient(new StreamxClientConfigImpl(config));
