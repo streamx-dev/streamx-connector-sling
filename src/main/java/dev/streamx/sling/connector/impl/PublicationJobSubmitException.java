3ea086fc88569bb996d2ddbe92d8b94df748c4f8
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-07-29 11:49:50 +0200

    fix

--- src/main/java/dev/streamx/sling/connector/impl/PublicationJobSubmitException.java
+++ src/main/java/dev/streamx/sling/connector/impl/PublicationJobSubmitException.java
@@ -2,7 +2,7 @@ package dev.streamx.sling.connector.impl;
 
 class PublicationJobSubmitException extends Exception {
 
-  public PublicationJobSubmitException(String message, Throwable cause) {
+  PublicationJobSubmitException(String message, Throwable cause) {
     super(message, cause);
   }
 }
